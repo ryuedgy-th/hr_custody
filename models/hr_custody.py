95993df33996e1e43110ac35aeaa4861abba35bd
ryuedgy-th <ryuedgyth@gmail.com>
2025-06-08 15:24:32 +0700

    add Return Date Rule

--- models/hr_custody.py
+++ models/hr_custody.py
@@ -95,13 +95,33 @@ class HrCustody(models.Model):
         help='The property associated with this custody record'
     )
 
+    # New fields for flexible return date management
+    return_type = fields.Selection([
+        ('date', 'Fixed Return Date'),
+        ('flexible', 'No Fixed Return Date'),
+        ('term_end', 'Return at Term/Project End')
+    ], string='Return Type', default='date', required=True, tracking=True,
+    help='Select the type of return date arrangement')
+
     return_date = fields.Date(
         string='Return Date',
-        required=True,
         tracking=True,
-        help='The date when the custody is expected to be returned.'
+        help='The date when the custody is expected to be returned (for fixed date only)'
     )
 
+    expected_return_period = fields.Char(
+        string='Expected Return Period',
+        help='E.g. "End of Term 1/2024", "When project finished", "As needed"'
+    )
+
+    return_status_display = fields.Char(
+        string='Return Status',
+        compute='_compute_return_status_display',
+        store=False,
+        help='Display return status in readable format'
+    )
+
+    # Existing fields
     renew_date = fields.Date(
         string='Renewal Return Date',
         tracking=True,
@@ -150,6 +170,24 @@ class HrCustody(models.Model):
         compute='_compute_is_read_only'
     )
 
+    # Computed Fields
+    @api.depends('return_type', 'return_date', 'expected_return_period', 'state')
+    def _compute_return_status_display(self):
+        """Compute return status display in readable format"""
+        for record in self:
+            if record.state == 'returned':
+                record.return_status_display = 'Returned'
+            elif record.return_type == 'date' and record.return_date:
+                record.return_status_display = f'Due: {record.return_date.strftime("%d/%m/%Y")}'
+            elif record.return_type == 'flexible':
+                period = record.expected_return_period or 'No fixed date'
+                record.return_status_display = f'Flexible ({period})'
+            elif record.return_type == 'term_end':
+                period = record.expected_return_period or 'End of term'
+                record.return_status_display = f'Return {period}'
+            else:
+                record.return_status_display = 'Pending'
+
     @api.depends('employee_id')
     def _compute_is_read_only(self):
         """ Use this function to check whether
@@ -162,56 +200,156 @@ class HrCustody(models.Model):
             else:
                 record.is_read_only = False
 
+    # Onchange Methods
+    @api.onchange('return_type')
+    def _onchange_return_type(self):
+        """Clear fields when changing return type"""
+        if self.return_type != 'date':
+            self.return_date = False
+        if self.return_type == 'date':
+            self.expected_return_period = False
+
+    # Constraint Methods
+    @api.constrains('return_type', 'return_date', 'expected_return_period', 'date_request')
+    def _check_return_requirements(self):
+        """Validate required fields based on return type"""
+        for record in self:
+            # Check for fixed date return type
+            if record.return_type == 'date':
+                if not record.return_date:
+                    raise ValidationError('Please specify return date when selecting "Fixed Return Date"')
+                if record.return_date < record.date_request:
+                    raise ValidationError('Return date must not be before request date')
+
+            # Check for flexible or term end return types
+            elif record.return_type in ['flexible', 'term_end']:
+                if not record.expected_return_period:
+                    raise ValidationError('Please specify expected return period')
+
+    @api.constrains('return_date', 'date_request')
+    def validate_return_date(self):
+        """The function validate the return
+        date to ensure it is after the request date"""
+        for record in self:
+            if record.return_date and record.date_request:
+                if record.return_date < record.date_request:
+                    raise ValidationError(_('Return date must be after request date'))
+
+    # Email and Reminder Methods
     def mail_reminder(self):
-        """ Use this function to product return reminder mail"""
+        """ Use this function to send return reminder mail"""
         now = datetime.now() + timedelta(days=1)
         date_now = now.date()
-        match = self.search([('state', '=', 'approved')])
 
-        for custody_record in match:
-            if custody_record.return_date:
-                exp_date = custody_record.return_date
-                if exp_date <= date_now:
-                    base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
-                    url = f"{base_url}/web#id={custody_record.id}&view_type=form&model=hr.custody"
+        # Send reminders for fixed date returns
+        fixed_date_records = self.search([
+            ('state', '=', 'approved'),
+            ('return_type', '=', 'date'),
+            ('return_date', '!=', False)
+        ])
 
-                    mail_content = _(
-                        'Hi %s,<br>As per the %s you took %s on %s for the reason of %s. So here we '
-                        'remind you that you have to return that on or before %s. Otherwise, you can '
-                        'renew the reference number(%s) by extending the return date through following '
-                        'link.<br> <div style = "text-align: center; margin-top: 16px;"><a href = "%s"'
-                        'style = "padding: 5px 10px; font-size: 12px; line-height: 18px; color: #FFFFFF; '
-                        'border-color:#875A7B;text-decoration: none; display: inline-block; '
-                        'margin-bottom: 0px; font-weight: 400;text-align: center; vertical-align: middle; '
-                        'cursor: pointer; white-space: nowrap; background-image: none; '
-                        'background-color: #875A7B; border: 1px solid #875A7B; border-radius:3px;">'
-                        'Renew %s</a></div>'
-                    ) % (
-                        custody_record.employee_id.name,
-                        custody_record.name,
-                        custody_record.custody_property_id.name,
-                        custody_record.date_request,
-                        custody_record.purpose,
-                        date_now,
-                        custody_record.name,
-                        url,
-                        custody_record.name
-                    )
+        for custody_record in fixed_date_records:
+            if custody_record.return_date <= date_now:
+                self._send_fixed_date_reminder(custody_record)
 
-                    main_content = {
-                        'subject': _('REMINDER On %s') % custody_record.name,
-                        'author_id': self.env.user.partner_id.id,
-                        'body_html': mail_content,
-                        'email_to': custody_record.employee_id.work_email,
-                    }
+        # Send reminders for flexible returns (every 30 days)
+        flexible_records = self.search([
+            ('state', '=', 'approved'),
+            ('return_type', 'in', ['flexible', 'term_end'])
+        ])
 
-                    mail_id = self.env['mail.mail'].create(main_content)
-                    mail_id.send()
+        for record in flexible_records:
+            if self._should_send_flexible_reminder(record):
+                self._send_flexible_reminder(record)
 
-                    if custody_record.employee_id.user_id:
-                        mail_id.mail_message_id.write({
-                            'partner_ids': [(4, custody_record.employee_id.user_id.partner_id.id)]
-                        })
+    def _send_fixed_date_reminder(self, custody_record):
+        """Send reminder for fixed date returns"""
+        base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
+        url = f"{base_url}/web#id={custody_record.id}&view_type=form&model=hr.custody"
+
+        mail_content = _(
+            'Hi %s,<br/>As per the %s you took %s on %s for the reason of %s.<br/>'
+            'Due date was %s which has passed.<br/><br/>'
+            'Please return the property as soon as possible. Otherwise, you can '
+            'renew the reference number(%s) by extending the return date through '
+            'following link.<br/><br/>'
+            '<div style = "text-align: center; margin-top: 16px;"><a href = "%s"'
+            'style = "padding: 5px 10px; font-size: 12px; line-height: 18px; color: #FFFFFF; '
+            'border-color:#875A7B;text-decoration: none; display: inline-block; '
+            'margin-bottom: 0px; font-weight: 400;text-align: center; vertical-align: middle; '
+            'cursor: pointer; white-space: nowrap; background-image: none; '
+            'background-color: #875A7B; border: 1px solid #875A7B; border-radius:3px;">'
+            'Renew %s</a></div>'
+        ) % (
+            custody_record.employee_id.name,
+            custody_record.name,
+            custody_record.custody_property_id.name,
+            custody_record.date_request,
+            custody_record.purpose,
+            custody_record.return_date,
+            custody_record.name,
+            url,
+            custody_record.name
+        )
+
+        main_content = {
+            'subject': _('REMINDER On %s') % custody_record.name,
+            'author_id': self.env.user.partner_id.id,
+            'body_html': mail_content,
+            'email_to': custody_record.employee_id.work_email,
+        }
+
+        mail_id = self.env['mail.mail'].create(main_content)
+        mail_id.send()
+
+    def _should_send_flexible_reminder(self, record):
+        """Check if should send flexible reminder (every 30 days)"""
+        last_message = self.env['mail.message'].search([
+            ('model', '=', 'hr.custody'),
+            ('res_id', '=', record.id),
+            ('subject', 'ilike', 'reminder'),
+        ], order='date desc', limit=1)
+
+        if not last_message:
+            return True
+
+        days_since_last = (fields.Date.today() - last_message.date.date()).days
+        return days_since_last >= 30
+
+    def _send_flexible_reminder(self, record):
+        """Send reminder for flexible return date items"""
+        mail_content = _(
+            'Hi %s,<br/>'
+            'This is a reminder about the %s you borrowed on %s.<br/>'
+            'Return Type: %s<br/>'
+            'Expected Return Period: %s<br/><br/>'
+            'If you can return it now, please contact HR department.<br/>'
+            'If you still need to use it, please inform us.<br/><br/>'
+            'Best regards,<br/>%s'
+        ) % (
+            record.employee_id.name,
+            record.custody_property_id.name,
+            record.date_request,
+            dict(record._fields['return_type'].selection)[record.return_type],
+            record.expected_return_period or 'Not specified',
+            record.company_id.name or 'HR Department'
+        )
+
+        main_content = {
+            'subject': _('Custody Reminder: %s') % record.custody_property_id.name,
+            'author_id': self.env.user.partner_id.id,
+            'body_html': mail_content,
+            'email_to': record.employee_id.work_email,
+        }
+
+        mail_id = self.env['mail.mail'].create(main_content)
+        mail_id.send()
+
+        # Post message for tracking
+        record.message_post(
+            body=_('Flexible return reminder sent to %s') % record.employee_id.name,
+            message_type='notification'
+        )
 
     @api.model_create_multi
     def create(self, vals_list):
@@ -276,16 +414,9 @@ class HrCustody(models.Model):
         """The function used to set the current
         custody record to the 'returned' state"""
         self.state = 'returned'
-        self.return_date = fields.Date.today()
-
-    @api.constrains('return_date', 'date_request')
-    def validate_return_date(self):
-        """The function validate the return
-        date to ensure it is after the request date"""
-        for record in self:
-            if record.return_date and record.date_request:
-                if record.return_date < record.date_request:
-                    raise ValidationError(_('Return date must be after request date'))
+        # Don't automatically set return_date for flexible returns
+        if self.return_type == 'date':
+            self.return_date = fields.Date.today()
 
     def unlink(self):
         """Override unlink to prevent deletion of approved records"""
