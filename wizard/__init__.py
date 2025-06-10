7e591d0b8e9cf809b2d52a93d1ad52809a9a0c80
ryuedgy-th <ryuedgyth@gmail.com>
2025-06-10 19:14:58 +0700

    Update wizard __init__.py to import all wizard models including missing ones

--- wizard/__init__.py
+++ wizard/__init__.py
@@ -1,1 +1,5 @@
 from . import custody_image_upload_wizard
+from . import property_return_reason
+from . import property_return_date
+from . import custody_before_wizard
+from . import custody_after_wizard
