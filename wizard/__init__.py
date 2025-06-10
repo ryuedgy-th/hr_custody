0ddd2e78f84925dfab647825bddfd2b3461f5a15
ryuedgy-th <ryuedgyth@gmail.com>
2025-06-10 19:02:28 +0700

    Add wizard init file for multiple image upload

--- wizard/__init__.py
+++ wizard/__init__.py
@@ -1,4 +1,1 @@
-from . import property_return_date
-from . import property_return_reason
-from . import custody_before_wizard
-from . import custody_after_wizard
+from . import custody_image_upload_wizard
