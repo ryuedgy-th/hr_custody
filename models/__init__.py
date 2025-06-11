d1fb8b5a3e9ea85e6702d69c272360b9676b77a1
ryuedgy-th <ryuedgyth@gmail.com>
2025-06-12 00:31:00 +0700

    🗑️ Remove image system completely - prepare for new image system
    REMOVED FILES:
    - models/custody_image.py
    - wizard/custody_before_wizard.py
    - wizard/custody_after_wizard.py
    - wizard/custody_image_upload_wizard.py
    - wizard/custody_before_wizard_views.xml
    - wizard/custody_after_wizard_views.xml
    - views/custody_image_views.xml
    - views/custody_image_upload_wizard_views.xml
    
    UPDATED FILES:
    - models/__init__.py - removed custody_image import
    - wizard/__init__.py - removed image wizard imports
    - models/hr_custody.py - removed all image-related fields and methods
    - __manifest__.py - removed image-related views and updated description
    
    This cleans up the entire image system to prepare for a new implementation.

--- models/__init__.py
+++ models/__init__.py
@@ -2,4 +2,3 @@ from . import property_category
 from . import custody_property
 from . import hr_custody
 from . import hr_employee
-from . import custody_image
--- wizard/__init__.py
+++ wizard/__init__.py
@@ -1,5 +1,2 @@
-from . import custody_image_upload_wizard
 from . import property_return_reason
 from . import property_return_date
-from . import custody_before_wizard
-from . import custody_after_wizard
