c9be12855429aa5a43f6de2d509af82c1323c5d1
ryuedgy-th <ryuedgyth@gmail.com>
2025-06-12 02:38:18 +0700

    Update wizard __init__.py to include photo testing wizard

--- wizard/__init__.py
+++ wizard/__init__.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+
 from . import property_return_reason
 from . import property_return_date
-from . import custody_photo_wizards  # 📸 NEW: Photo management wizards
+from . import custody_before_wizard
+from . import custody_after_wizard
+from . import custody_image_upload_wizard
+from . import custody_photo_wizards
+from . import hr_custody_photo_tests  # 🧪 NEW: Photo testing wizard
